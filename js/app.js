a1034aa4e34648fb9c15a6a1aa7fbc1d56db2250
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-01 17:35:11 -0600

    Okay. Javascript is actually lit.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-// let word = "fantastic"
+/*
+This is how I will build hangman.
+Let's have a text field that the user can type in one letter at a time.
+For now we can count strikes as a number.
+For the letters if we guess the word right then we can add a div or something.
+*/
 
-// document.getElementById("title");
 
-// let first = document.createElement("p");
-// // let text = document.createTextNode()
-// first.appendChild(word);
-// let element = document.getElementById("title");
-// element.appendChild(first);
+function addElement(el, val, id)   {
+    // This creates an element, and whatever val is passed in is what the tag gets.
+    var tag = document.createElement(el);
+    var value = document.createTextNode(val);
+    tag.appendChild(value);
 
-// console.log(word);
+    // This adds the element into the document.
+    var element = document.getElementById(id);
+    element.appendChild(tag);
+};
 
-var tag = document.createElement("p");
-var text = document.createTextNode("Tutorix is the best e-learning platform");
-tag.appendChild(text);
-var element = document.getElementById("title");
-element.appendChild(tag);
+// here we call the function, and all we need to provide is the tag, textvalue,
+//  and the id so that it knows where to be placed in the document.
+addElement("p", "will this work?", "word");
