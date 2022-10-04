6eea7372e761a8d11fa97b881ea355402b301a6e
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-03 22:12:33 -0600

    Okay cool

--- js/app.js
+++ js/app.js
@@ -22,13 +22,51 @@ function addElement(el, val, id)   {
 //  and the id so that it knows where to be placed in the document.
 // addElement("p", "will this work?", "word");
 
-let wordToGuess = "horse";
+let wordToGuess = "HORSE";
+
+
+function selectChild()  {
+    document.getElementById('firstRow').addEventListener('click', (e) => {
+        let guessedChar = e.target.innerHTML;
+        // console.log(e.target.style);
+        // if (e.target.innerHTML == 'Q')  {
+        //     // compare this letter to the wordToGuess string, and then add it up top, or add a strike
+        //     console.log('q was pressed');
+        // }
+        // console.log(guessedChar);
+
+        // for (let i = 0; i < wordToGuess.length; i++)  {
+        //     // console.log(wordToGuess[i]);
+        //     // console.log(guessedChar);
+
+        //     if (guessedChar === wordToGuess[i])  {
+        //         console.log("found a char");
+        //     }   else    {
+        //         console.log("no match");
+        //     }
+        // }        
+        // console.log(e.target.innerHTML);
+        if (wordToGuess.includes(guessedChar))  {
+            console.log("true");
+            // let hideGuess = document.querySelector('p')
+            e.target.style.backgroundColor = 'green';
+            e.target.style.pointerEvents = 'none';
+        }   else    {
+            
+            e.target.style.backgroundColor = 'red';
+            console.log("false");
+        }
+    });
+    // console.log('child selected');
+}
 
 // Choose a char, and depending on the guessing word we will add it up top to the correct chars, or add a strike.
 function selectChar()   {
     // we select the parent div id, and then we select all of the p tags in that parent div.
     let firstCharRow = document.querySelectorAll('#firstRow p');
     // console.log(firstCharRow);
+
+
 
     for (let i = 0; i < firstCharRow.length; i++) {
         // console.log("1")
@@ -42,13 +80,14 @@ function selectChar()   {
         // }
     }
 
-    if (firstCharRow[0].innerHTML == 'Q')   {
-        console.log(firstCharRow[0].innerHTML);
-        console.log('q was selected');
-    }
-    else {
-        console.log("char select success");
-    }
+    // if (firstCharRow[0].innerHTML == 'Q')   {
+    //     console.log(firstCharRow[0].innerHTML);
+    //     console.log('q was selected');
+    // }
+    // else {
+    //     console.log("char select success");
+    // }
+    console.log('clicked');
 }
 // If letter is in the word we need to guess, make the letter we clicked green, and add it up top
 // if letter isn't in the word then make it red, and update strikes.
